7a6c752f402cac198115e1a156b8efedd0258a80
Stepan1983 <80103271+Stepan1983@users.noreply.github.com>
2023-08-11 11:25:58 +0000

    Initial commit

---
+++ images/videos.php
@@ -0,0 +1,213 @@
+<?php
+
+$videoId = "JhYgXMAzOXs";
+$videoTitle = "СМЕШНЫЕ КОШКИ 2023 И ДРУГИЕ ЖИВОТНЫЕ/ 10 МИНУТ СМЕХА/СМЕШНЫЕ ЖИВОТНЫЕ 2023/ЛУЧШИЕ ПРИКОЛЫ С КОТАМИ";
+$videoDescription = "НА ПУШИСТОМ КАНАЛЕ HAPPY DAY, ВЫ УВИДИТЕ САМЫЕ СМЕШНЫЕ ЖИВОТНЫЕ ОТ КОТОРЫХ БУДИТЕ РЖАТЬ ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "C0gK-NKEmno";
+$videoTitle = "СМЕШНЫЕ КОШКИ 2023 И ДРУГИЕ ЖИВОТНЫЕ/ 10 МИНУТ СМЕХА/СМЕШНЫЕ ЖИВОТНЫЕ 2023/ЛУЧШИЕ ПРИКОЛЫ С КОТАМИ";
+$videoDescription = "НА ПУШИСТОМ КАНАЛЕ HAPPY DAY, ВЫ УВИДИТЕ САМЫЕ СМЕШНЫЕ ЖИВОТНЫЕ ОТ КОТОРЫХ БУДИТЕ РЖАТЬ ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "dLw6aqKbpsg";
+$videoTitle = "Самые смешные животные / Приколы с котами и собаками 2022 / LA #59";
+$videoDescription = "Самые смешные животные / Приколы с котами и собаками 2022 / LA #59 ⭐Забавные Животные: ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "jhJf69-1gvI";
+$videoTitle = "Самые смешные животные / Приколы с котами и собаками 2023 / LA #160";
+$videoDescription = "Самые смешные животные / Приколы с котами и собаками 2023 / LA #160 ⭐Забавные Животные: ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "uQhvuVx3lDQ";
+$videoTitle = "СМЕШНЫЕ КОШКИ 2023 И ДРУГИЕ ЖИВОТНЫЕ/ 10 МИНУТ СМЕХА/СМЕШНЫЕ ЖИВОТНЫЕ 2023/ЛУЧШИЕ ПРИКОЛЫ С КОТАМИ";
+$videoDescription = "НА ПУШИСТОМ КАНАЛЕ HAPPY DAY, ВЫ УВИДИТЕ САМЫЕ СМЕШНЫЕ ЖИВОТНЫЕ ОТ КОТОРЫХ БУДИТЕ РЖАТЬ ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "-L46fs7Pukw";
+$videoTitle = "ЕСЛИ БЫ КОТИКИ ХОДИЛИ В ШКОЛУ - ЛУЧШИЕ СЕРИИ";
+$videoDescription = "В этом выпуске ученики школы котиков будут смотреть самые любимые серии. Кому какая серия нравится больше?";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "eGuP03kvxGY";
+$videoTitle = "🐈Идеальное преступление! 😺 Подборка смешных котов и котят для хорошего настроения! 😻";
+$videoDescription = "Сегодня в выпуске: Банда кошек, подножка для человека! Подписывайтесь на канал, ставьте лайки!";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "ASUeDheZxn4";
+$videoTitle = "ЕСЛИ БЫ КОТИКИ ХОДИЛИ В ШКОЛУ - DOORS РОБЛОКС";
+$videoDescription = "В этом выпуске Кусев решил поиграть в Doors прямо на уроке! На минутку он даже забылся, что сидит в классе, каково же ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "F-T30lOAwRw";
+$videoTitle = "СМЕШНЫЕ КОШКИ 2023 И ДРУГИЕ ЖИВОТНЫЕ/ 10 МИНУТ СМЕХА/СМЕШНЫЕ ЖИВОТНЫЕ 2023/ЛУЧШИЕ ПРИКОЛЫ С КОТАМИ";
+$videoDescription = "НА ПУШИСТОМ КАНАЛЕ HAPPY DAY, ВЫ УВИДИТЕ САМЫЕ СМЕШНЫЕ ЖИВОТНЫЕ ОТ КОТОРЫХ БУДИТЕ РЖАТЬ ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "YT8_mOb3fJU";
+$videoTitle = "44 Котёнка | 300 МИНУТ полных серий [ПОЛНЫЕ СЕРИИ]";
+$videoDescription = "Подпишись сейчас: http://bit.ly/3317atB WEBSITE: https://www.44cats.tv VK: https://vk.com/44catsofficialrussian INSTAGRAM: ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "IW8BJZCdLv8";
+$videoTitle = "Самые смешные животные / Приколы с котами и собаками 2022 / LA #72";
+$videoDescription = "Самые смешные животные / Приколы с котами и собаками 2022 / LA #72 ⭐Забавные Животные: ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "MnAMmPCiurQ";
+$videoTitle = "Самые смешные животные / Приколы с котами и собаками 2023 / LA #157";
+$videoDescription = "Самые смешные животные / Приколы с котами и собаками 2023 / LA #157 ⭐Забавные Животные: ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "YJw07eWR1zg";
+$videoTitle = "СМЕШНЫЕ КОШКИ 2023 И ДРУГИЕ ЖИВОТНЫЕ/ 10 МИНУТ СМЕХА/СМЕШНЫЕ ЖИВОТНЫЕ 2023/ЛУЧШИЕ ПРИКОЛЫ С КОТАМИ";
+$videoDescription = "НА ПУШИСТОМ КАНАЛЕ HAPPY DAY, ВЫ УВИДИТЕ САМЫЕ СМЕШНЫЕ ЖИВОТНЫЕ ОТ КОТОРЫХ БУДИТЕ РЖАТЬ ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "Bo6d24MzD3s";
+$videoTitle = "СМЕШНЫЕ КОШКИ 2023 И ДРУГИЕ ЖИВОТНЫЕ/ 10 МИНУТ СМЕХА/СМЕШНЫЕ ЖИВОТНЫЕ 2023/ЛУЧШИЕ ПРИКОЛЫ С КОТАМИ";
+$videoDescription = "НА ПУШИСТОМ КАНАЛЕ HAPPY DAY, ВЫ УВИДИТЕ САМЫЕ СМЕШНЫЕ ЖИВОТНЫЕ ОТ КОТОРЫХ БУДИТЕ РЖАТЬ ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "2vNuyySoBgs";
+$videoTitle = "Самые смешные животные / Приколы с котами и собаками 2022 / LA #108";
+$videoDescription = "Самые смешные животные / Приколы с котами и собаками 2022 / LA #108 ⭐Забавные Животные: ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "PlCZPSjTQjc";
+$videoTitle = "СМЕШНЫЕ КОШКИ 2023 И ДРУГИЕ ЖИВОТНЫЕ/ 10 МИНУТ СМЕХА/СМЕШНЫЕ ЖИВОТНЫЕ 2023/ЛУЧШИЕ ПРИКОЛЫ С КОТАМИ";
+$videoDescription = "НА ПУШИСТОМ КАНАЛЕ HAPPY DAY, ВЫ УВИДИТЕ САМЫЕ СМЕШНЫЕ ЖИВОТНЫЕ ОТ КОТОРЫХ БУДИТЕ РЖАТЬ ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "tFyuC07WJEE";
+$videoTitle = "Коты трюкачи 😺 #симба #симбочка #анимация";
+$videoDescription = "Спасибо за лайк и подписку :3 Скачай мою вирусную игру "Спаси Симбочку": https://pimpochka.games/save-simbachka Мой ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "HjCCp678clo";
+$videoTitle = "🐈 Кошачьи фокусы! 🐕 Видео смешных котов и котят для хорошего настроения! 😻";
+$videoDescription = "Сегодня в выпуске: украсть кольцо, покажи когти! Подписывайтесь на канал, ставьте лайки!";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "UX3R32SzU8U";
+$videoTitle = "Самые смешные животные / Приколы с котами и собаками 2023 / LA #159";
+$videoDescription = "Самые смешные животные / Приколы с котами и собаками 2023 / LA #159 ⭐Забавные Животные: ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "0X8Jm-r6Q_o";
+$videoTitle = "Шок! Кошки умеют спать с открытыми глазами! #коты #кот #кошка";
+$videoDescription = "";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "qiV9bicTVrg";
+$videoTitle = "СМЕШНЫЕ КОШКИ 2023 И ДРУГИЕ ЖИВОТНЫЕ/ 10 МИНУТ СМЕХА/СМЕШНЫЕ ЖИВОТНЫЕ 2023/ЛУЧШИЕ ПРИКОЛЫ С КОТАМИ";
+$videoDescription = "НА ПУШИСТОМ КАНАЛЕ HAPPY DAY, ВЫ УВИДИТЕ САМЫЕ СМЕШНЫЕ ЖИВОТНЫЕ ОТ КОТОРЫХ БУДИТЕ РЖАТЬ ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "pa4b57fOX_c";
+$videoTitle = "Самые смешные животные 😂 Приколы с котами и собаками 2023 😺🐶 Часть 18";
+$videoDescription = "Самые смешные животные Приколы с котами и собаками 2023 Часть 18 Забавные Животные: ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "ByLmQz7bTF4";
+$videoTitle = "Самые смешные животные / Приколы с котами и собаками 2023 / LA #156";
+$videoDescription = "Самые смешные животные / Приколы с котами и собаками 2023 / LA #156 ⭐Забавные Животные: ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "aDjKL5ZCA9w";
+$videoTitle = "Я РЖАЛ ЦЕЛЫЙ ЧАС😹КОШКИ 2022 ЛУЧШИЕ ПРИКОЛЫ/ СМЕШНЫЕ ЖИВОТНЫЕ ЗА ВЕСЬ ЯНВАРЬ 2022😹";
+$videoDescription = "НА ПУШИСТОМ КАНАЛЕ HAPPY DAY, ВЫ УВИДИТЕ САМЫЕ СМЕШНЫЕ ЖИВОТНЫЕ ОТ КОТОРЫХ БУДИТЕ РЖАТЬ ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "efEAMnkGeco";
+$videoTitle = "УЖАСНАЯ ТАЙНА, КОТЫ ПОДГЛЯДЫВАЮТ ИЗ-ЗА УГЛА #shorts";
+$videoDescription = "Интересно, кстати не забудь подписаться на канал! #шортс #факты #коты.";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "TfYm7TPadTY";
+$videoTitle = "Самые смешные животные / Приколы с котами и собаками 2023 / LA #136";
+$videoDescription = "Самые смешные животные / Приколы с котами и собаками 2023 / LA #136 ⭐Забавные Животные: ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "KIFyC6i6uTU";
+$videoTitle = "Когда отмечают день котиков #коты #кот";
+$videoDescription = "";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "CXvJn8kKfHc";
+$videoTitle = "В каких цветах видят мир кошки #кошка #кот #коты";
+$videoDescription = "";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "RQa1GTCONhE";
+$videoTitle = "Приколы с котами! Самые Смешные животные С ОЗВУЧКОЙ! ДО СЛЁЗ!";
+$videoDescription = "Приколы с котами с озвучкой рассмешат вас до слёз! Смешные коты попадают в разные ситуации. Смешная озвучка котов ...";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+$videoId = "iyORq1VMwXw";
+$videoTitle = "Самое строгое наказание, которым кошки наказывают котят  #кошка #кот #коты";
+$videoDescription = "";
+echo '<h2>' . $videoTitle . '</h2>';
+echo '<p>' . $videoDescription . '</p>';
+echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allowfullscreen></iframe>';
+
+?>
